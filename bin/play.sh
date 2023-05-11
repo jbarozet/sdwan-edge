d47d45800f2dd03df5204184eb84a42f8462012e
Jean-Marc Barozet <jbarozet@gmail.com>
2023-05-11 10:44:15 +0200

    update PROJ_ROOT definition

--- bin/play.sh
+++ bin/play.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-PROJ_ROOT="/Users/jbarozet/Documents/Code/sdwan-edge"
+PROJ_ROOT="$PWD/.."
 IMAGE="sdwan-container"
 OPTIONS=
 
 while getopts ":r" opt; do
     case $opt in
     r)
-        docker run -it -v $PROJ_ROOT/ansible:/ansible -v $HOME/.aws:/root/.aws $IMAGE /bin/bash
+        docker run -it \
+            -v $PROJ_ROOT/ansible:/ansible \
+            -v $PROJ_ROOT/provision_edge:/provision_edge \
+            -v $PROJ_ROOT/provision_vpc:/provision_vpc \
+            -v $HOME/.aws:/root/.aws $IMAGE /bin/bash
         exit
         ;;
     esac
